373eaa73a0ef19d716edfece6e57162590e41735
emilia_vv <emilka.wojcik2002@gmail.com>
2024-01-08 00:13:55 +0100

    added real data -  time and date

---
+++ src/index.js
@@ -0,0 +1,15 @@
+setInterval(()=> {
+let paris = document.querySelector("#paris")
+let parisDate = paris.querySelector(".date")
+let place1 = moment().tz("Europe/Paris")
+parisDate.innerHTML = place1.format("dddd, Do MMM YYYY")
+parisTime = paris.querySelector(".time")
+parisTime.innerHTML = place1.format("h:mm:ss [<small>]A[</small>]")
+
+let lisbon = document.querySelector("#lisbon")
+let lisbonDate = lisbon.querySelector(".date")
+let place2 = moment().tz("Europe/Lisbon")
+lisbonDate.innerHTML = place2.format("dddd, Do MMM YYYY")
+lisbonTime = lisbon.querySelector(".time")
+lisbonTime.innerHTML = place2.format("h:mm:ss [<small>]A[</small>]")
+}, 1000)
