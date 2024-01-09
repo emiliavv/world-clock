e4460d296342018ce5bdd97b42adb7489d9cd53a
emilia_vv <emilka.wojcik2002@gmail.com>
2024-01-09 20:39:40 +0100

    added possibility of removing added cities

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
 setInterval(()=> {
-let paris = document.querySelector("#paris")
-let parisDate = paris.querySelector(".date")
-let place1 = moment().tz("Europe/Paris")
-parisDate.innerHTML = place1.format("dddd, Do MMM YYYY")
-parisTime = paris.querySelector(".time")
-parisTime.innerHTML = place1.format("h:mm:ss [<small>]A[</small>]")
-
-let lisbon = document.querySelector("#lisbon")
-let lisbonDate = lisbon.querySelector(".date")
-let place2 = moment().tz("Europe/Lisbon")
-lisbonDate.innerHTML = place2.format("dddd, Do MMM YYYY")
-lisbonTime = lisbon.querySelector(".time")
-lisbonTime.innerHTML = place2.format("h:mm:ss [<small>]A[</small>]")
+let krakow = document.querySelector("#krakow")
+let krakowDate = krakow.querySelector(".date")
+let place = moment().tz("Poland")
+krakowDate.innerHTML = place.format("dddd, Do MMM YYYY")
+krakowTime = krakow.querySelector(".time")
+krakowTime.innerHTML = place.format("h:mm:ss [<small>]A[</small>]")
 }, 1000)
 
 
 function addTimes(event) {
     let selectedCity = event.target.value;
     let place = moment().tz(selectedCity);
-    let clocks = document.querySelector(".clocks")
-    clocks.innerHTML += `
-    <div class="times">
-            <div class="citie">
-            <div class="button"><button>-</button></div>
-            <div class="place">${event.target.value.replace("_", " ").split("/")[1]}, ${event.target.value.replace("_", " ").split("/")[0]}</div>
-            <div class="date">${place.format("dddd, Do MMM YYYY")}</div>
-            <div class="time" id="${selectedCity}">${place.format("h:mm:ss [<small>]A[</small>]")}</div>
-    </div>
+    let times = document.querySelector(".times")
+    times.innerHTML += `
+        <div class="citie">
+        <div class="button" id="button-${selectedCity}"><button>-</button></div>
+        <div class="place">${event.target.value.replace("_", " ").split("/")[1]}, ${event.target.value.replace("_", " ").split("/")[0]}</div>
+        <div class="date">${place.format("dddd, Do MMM YYYY")}</div>
+        <div class="time" id="${selectedCity}">${place.format("h:mm:ss [<small>]A[</small>]")}</div>
    `
     setInterval(() => {
         let currentTime = moment().tz(selectedCity);
         document.getElementById(selectedCity).innerHTML = currentTime.format("h:mm:ss [<small>]A[</small>]");
         }, 1000);
 }
-let c
-hooseCity = document.querySelector("#chooseCity")
+
+let chooseCity = document.querySelector("#chooseCity")
 chooseCity.addEventListener("change", addTimes)
+
+function removeTime(event) {
+    let selectedCity = event.target.parentElement.id.replace("button-", "");
+    let clockToRemove = document.getElementById(selectedCity);
+    clockToRemove.parentElement.remove();
+}
+
+// Add the removeTime function to the click event of the button
+let clocksContainer = document.querySelector(".clocks");
+clocksContainer.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON") {
+        removeTime(event);
+    }
+});
