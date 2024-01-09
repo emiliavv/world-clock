09b9ede1d4003d84bda247798a2a75f843c6cd29
emilia_vv <emilka.wojcik2002@gmail.com>
2024-01-09 18:47:05 +0100

    add city's time to main page

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ lisbonDate.innerHTML = place2.format("dddd, Do MMM YYYY")
 lisbonTime = lisbon.querySelector(".time")
 lisbonTime.innerHTML = place2.format("h:mm:ss [<small>]A[</small>]")
 }, 1000)
+
+
+function addTimes(event) {
+    let place = moment().tz(event.target.value)
+    let clocks = document.querySelector(".clocks")
+    clocks.innerHTML += `
+    <div class="times">
+            <div class="citie" id="paris">
+            <div class="place">${event.target.value.replace("_", " ").split("/")[1]}</div>
+            <div class="date">${place.format("dddd, Do MMM YYYY")}</div>
+            <div class="time">${place.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>
+   `
+}
+let chooseCity = document.querySelector("#chooseCity")
+chooseCity.addEventListener("change", addTimes)
