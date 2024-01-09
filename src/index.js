2efc486becc064de0f5ade3440765be241076e17
emilia_vv <emilka.wojcik2002@gmail.com>
2024-01-09 21:10:20 +0100

    added your timezon option and reseting select option after choosing

--- src/index.js
+++ src/index.js
@@ -9,22 +9,29 @@ krakowTime.innerHTML = place.format("h:mm:ss [<small>]A[</small>]")
 
 
 function addTimes(event) {
-    let selectedCity = event.target.value;
-    let place = moment().tz(selectedCity);
-    let times = document.querySelector(".times")
-    if (document.getElementById(`${event.target.value}`) === null) {
-         times.innerHTML += `
-            <div class="citie">
-            <div class="button" id="button-${selectedCity}"><button>-</button></div>
-            <div class="place">${event.target.value.replace("_", " ").split("/")[1]}, ${event.target.value.replace("_", " ").split("/")[0]}</div>
-            <div class="date">${place.format("dddd, Do MMM YYYY")}</div>
-            <div class="time" id="${selectedCity}">${place.format("h:mm:ss [<small>]A[</small>]")}</div>
-        `
-        setInterval(() => {
-            let currentTime = moment().tz(selectedCity);
-            document.getElementById(selectedCity).innerHTML = currentTime.format("h:mm:ss [<small>]A[</small>]");
-            }, 1000);   
+    if (event.target.value !== 'choose') {
+        let selectedCity = event.target.value;
+        if (selectedCity === "guess") {
+            selectedCity = moment.tz.guess()
         }
+        let place = moment().tz(selectedCity);
+        let times = document.querySelector(".times")
+        if (document.getElementById(`${selectedCity}`) === null) {
+            times.innerHTML += `
+                <div class="citie">
+                <div class="button" id="button-${selectedCity}"><button>-</button></div>
+                <div class="place">${selectedCity.replace("_", " ").split("/")[1]}, ${selectedCity.replace("_", " ").split("/")[0]}</div>
+                <div class="date">${place.format("dddd, Do MMM YYYY")}</div>
+                <div class="time" id="${selectedCity}">${place.format("h:mm:ss [<small>]A[</small>]")}</div>
+            `
+            setInterval(() => {
+                let currentTime = moment().tz(selectedCity);
+                document.getElementById(selectedCity).innerHTML = currentTime.format("h:mm:ss [<small>]A[</small>]");
+                }, 1000);   
+            }
+    }
+    let chooseCity = document.querySelector("#chooseCity")
+    chooseCity.selectedIndex = 0;
 }
 
 let chooseCity = document.querySelector("#chooseCity")
